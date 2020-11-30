4df0bd968cba6595b06b9806f2db0d2af62e8d80
Alex Petitjean <alex@tenforwardconsulting.com>
2020-11-30 17:45:28 -0500

    Add args for passing in day number

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
     println("Hello Advent Of Code!")
+    val arg1 = args.toList().first()
+    val day = arg1.toIntOrNull()
+
+    if (day != null) {
+        println("You chose day $day")
+    } else {
+        println("Uh oh, \"$arg1\" is not a day >:(")
+    }
 }
