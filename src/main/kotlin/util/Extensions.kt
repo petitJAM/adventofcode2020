3cd93d73e5f94191a80d876cf12704162cd78a3c
Alex Petitjean <alex@tenforwardconsulting.com>
2020-12-02 10:49:47 -0500

    Add docs to helper extensions

--- src/main/kotlin/util/Extensions.kt
+++ src/main/kotlin/util/Extensions.kt
@@ -1,11 +1,20 @@
 package util
 
+/**
+ * The first element of the list.
+ */
 val <T> List<T>.head: T
     get() = first()
 
+/**
+ * Everything except the first element of the list.
+ */
 val <T> List<T>.tail: List<T>
     get() = drop(1)
 
+/**
+ * Split this [String] on the last occurrence of [delimiter].
+ */
 fun String.splitLast(delimiter: String): Pair<String, String> =
     lastIndexOf(delimiter).let { index ->
         substring(0, index) to substring(index + 1)
