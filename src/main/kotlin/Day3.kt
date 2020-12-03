a2dfb8bab55bfb21302ce8209915dc3d44d0ae5d
Alex Petitjean <alex@tenforwardconsulting.com>
2020-12-03 11:07:21 -0500

    Day 3 Part 2 solution

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -17,31 +17,64 @@ fun day3() {
     """.trimIndent()
         .split("\n")
 
-    println(input2.joinToString("\n"))
-
     val treeMap = parseInput(input)
 
     val answer = countTreesOnPath(treeMap, Slope(3, 1))
     println("The answer to part 1 is ... $answer")
 
-    val answer2 = -1
+    val slopes = listOf(
+        Slope(1, 1),
+        Slope(3, 1),
+        Slope(5, 1),
+        Slope(7, 1),
+        Slope(1, 2),
+    )
+
+    val counts = slopes
+        .map { slope ->
+            countTreesOnPath(treeMap, slope)
+        }
+        .map(Int::toBigInteger)
+
+    println(counts)
+
+    println(Int.MAX_VALUE)
+    val answer2 = counts.reduce { acc, i -> println("acc $acc * i $i = ${acc * i}"); acc * i }
     println("The answer to part 2 is ... $answer2")
 }
 
-// Part 1
+// Part 1+2
 
 private fun countTreesOnPath(treeMap: TreeMap, slope: Slope): Int {
     val width = treeMap.first().size
+    val height = treeMap.size
+
+    println("Slope ${slope.first}, ${slope.second}")
+    println("Width $width, Height $height")
+    println()
 
     var treeCount = 0
     var currentX = 0
     var currentY = 0
 
-    treeMap.forEach { row ->
-        if (row[currentX] == Square.TREE) treeCount += 1
+    while (currentY < height) {
+//        println("Checking $currentX, $currentY ... it's ${treeMap[currentY][currentX]}")
+        println(treeMap[currentY].toString(currentX))
+
+        if (slope.second > 1) {
+            var yBetween = currentY + 1
+            while (yBetween < currentY + slope.second && yBetween < height) {
+                println(treeMap[yBetween].toString(-1))
+                yBetween += 1
+            }
+        }
+
+        if (treeMap[currentY][currentX] == Square.TREE) treeCount += 1
         currentX = (currentX + slope.first) % width
-        currentY += 1
+        currentY += slope.second
     }
+
+    println()
 
     return treeCount
 }
@@ -60,6 +93,23 @@ private typealias Slope = Pair<Int, Int>
 private typealias TreeMap = List<TreeMapRow>
 
 private typealias TreeMapRow = List<Square>
+
+private fun TreeMapRow.toString(tobboganAt: Int): String =
+    this
+        .mapIndexed { index, square ->
+            if (index == tobboganAt) {
+                when (square) {
+                    Square.OPEN -> 'O'
+                    Square.TREE -> 'X'
+                }
+            } else {
+                when (square) {
+                    Square.OPEN -> '.'
+                    Square.TREE -> '#'
+                }
+            }
+        }
+        .joinToString("")
 
 private enum class Square {
     OPEN,
