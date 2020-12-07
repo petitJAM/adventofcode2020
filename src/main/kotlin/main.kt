05aaeda2f80e29bfc391187bbdc0e7d5ce878d15
Alex Petitjean <alex@tenforwardconsulting.com>
2020-12-07 10:14:08 -0600

    Use reflection to find latest day

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,25 +1,42 @@
-import java.io.File
+import java.lang.reflect.Method
+import java.lang.reflect.Modifier
 
 fun main(args: Array<String>) {
     println("Hello Advent Of Code!")
     val arg1 = args.toList().firstOrNull()
-    val day = arg1?.toIntOrNull() ?: 6
+    val day = arg1?.toIntOrNull()
+
+    val dayFunctions = (1..25).map { functionForDay(it) }
 
     if (day != null) {
-        println()
-        println("Running Day $day")
-
-        when (day) {
-            1 -> day1()
-            2 -> day2()
-            3 -> day3()
-            4 -> day4()
-            5 -> day5()
-            6 -> day6()
-            else -> println("That day isn't implemented yet! :(")
+        val func = dayFunctions[day - 1]
+        if (func != null) {
+            println("Running Day $day")
+            func.invoke(null)
+        } else {
+            println("That day isn't implemented yet! :(")
         }
-
     } else {
-        println("Uh oh, \"$arg1\" is not a day >:(")
+        val lastImplementedIndex = dayFunctions.filterNotNull().lastIndex
+        val currentDayFunc = dayFunctions[lastImplementedIndex]
+        if (currentDayFunc != null) {
+            println("Running Day ${lastImplementedIndex + 1}")
+            currentDayFunc.invoke(null)
+        } else {
+            println("Uh oh, there aren't any implemented days yet :(")
+        }
     }
+}
+
+private fun functionForDay(day: Int): Method? {
+    val fileName = "Day$day"
+    val funcName = "day$day"
+    val selfRef = ::functionForDay
+    val currentClass = selfRef.javaClass
+    val classDefiningFunctions = try {
+        currentClass.classLoader.loadClass("${fileName}Kt")
+    } catch (ex: ClassNotFoundException) {
+        null
+    }
+    return classDefiningFunctions?.methods?.find { it.name == funcName && Modifier.isStatic(it.modifiers) }
 }
