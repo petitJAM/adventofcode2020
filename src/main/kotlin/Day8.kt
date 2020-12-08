8a6f10800b8c879b54119315021dbc77cc61843a
Alex Petitjean <alex@tenforwardconsulting.com>
2020-12-08 10:18:32 -0600

    Day 8 Part 2 solution

--- src/main/kotlin/Day8.kt
+++ src/main/kotlin/Day8.kt
@@ -5,27 +5,86 @@ import util.splitLast
 @Suppress("unused")
 fun day8() {
     val input = readInputFile(8)
-    val input2 = Input(
-        """
-            nop +0
-            acc +1
-            jmp +4
-            acc +3
-            jmp -3
-            acc -99
-            acc +1
-            jmp -4
-            acc +6
-    """.trimIndent()
-    )
+//    val input2 = Input(
+//        """
+//            nop +0
+//            acc +1
+//            jmp +4
+//            acc +3
+//            jmp -3
+//            acc -99
+//            acc +1
+//            jmp -4
+//            acc +6
+//    """.trimIndent()
+//    )
 
     val instructions = parseInput(input)
 
     val answer = runUntilRepeatedInstruction(instructions)
     println("The answer to part 1 is ... $answer")
 
-    val answer2 = -1
+    val jmpNopIndices = instructions.mapIndexedNotNull { index, instruction ->
+        if (instruction.operation == Operation.JMP || instruction.operation == Operation.NOP) {
+            index
+        } else {
+            null
+        }
+    }
+
+    val instructionVariants = jmpNopIndices.map { index ->
+        instructions
+            .toMutableList()
+            .apply {
+                val instruction = removeAt(index)
+                val newOp = when (instruction.operation) {
+                    Operation.JMP -> Operation.NOP
+                    Operation.NOP -> Operation.JMP
+                    Operation.ACC -> throw IllegalStateException("Should not be trying to replace an ACC operation")
+                }
+                add(index, instruction.copy(operation = newOp))
+            }
+            .toList()
+    }
+
+    val results = instructionVariants.mapNotNull { instructions ->
+        runUntilRepeatedOrCompleted(instructions)
+    }
+
+    val answer2 = results.first()
     println("The answer to part 2 is ... $answer2")
+}
+
+// Part 2
+
+/**
+ * @return null if the program hit a repeated instruction or accumulator if the program completed successfully
+ */
+private fun runUntilRepeatedOrCompleted(instructions: List<Instruction>): Int? {
+    var accumulator = 0
+    val visitedInstructionIndices = mutableSetOf<Int>()
+    var currentPosition = 0
+
+    while (currentPosition !in visitedInstructionIndices && currentPosition < instructions.size) {
+        visitedInstructionIndices += currentPosition
+        val instruction = instructions[currentPosition]
+        when (instruction.operation) {
+            Operation.ACC -> {
+                accumulator += instruction.argument
+                currentPosition += 1
+            }
+            Operation.JMP -> {
+                currentPosition += instruction.argument
+            }
+            Operation.NOP -> currentPosition += 1
+        }
+    }
+
+    return if (currentPosition > instructions.lastIndex) {
+        accumulator
+    } else {
+        null
+    }
 }
 
 // Part 1
