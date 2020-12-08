fd3ac4bed43a335dd0397d1025ebf337052f0a8c
Alex Petitjean <alex@tenforwardconsulting.com>
2020-12-08 09:25:07 -0600

    Add script for generating day files

---
+++ new_day.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DAY_NUM=$1
+
+echo "Creating day $DAY_NUM"
+
+touch "src/main/resources/inputs/day$DAY_NUM.txt"
+
+KT_FILE="src/main/kotlin/Day$DAY_NUM.kt"
+
+if [[ ! -f "$KT_FILE" ]]; then
+  cat <<KT >> $KT_FILE
+import util.Input
+import util.readInputFile
+
+@Suppress("unused")
+fun day$DAY_NUM() {
+    val input = readInputFile($DAY_NUM)
+    val input2 = Input(
+        """
+    """.trimIndent()
+    )
+
+    val answer = -1
+    println("The answer to part 1 is ... \$answer")
+
+    val answer2 = -1
+    println("The answer to part 2 is ... \$answer2")
+}
+KT
+fi
