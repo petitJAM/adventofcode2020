db103e600f7a7a0813eee011f930794f28690e6a
Alex Petitjean <alex@tenforwardconsulting.com>
2020-12-09 09:36:35 -0600

    Day 9 Part 2 solution

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -37,9 +37,43 @@ fun day9() {
     val answer = firstInvalidNumber(input.toBigIntegerList(), 25)
     println("The answer to part 1 is ... $answer")
 
-    val answer2 = -1
+    val exampleAnswer2 = findContiguousSetThatSums(input2.toBigIntegerList(), exampleAnswer)
+        .sumOfSmallestAndLargest()
+    println("The example answer to part 2 is ... $exampleAnswer2")
+
+    val answer2 = findContiguousSetThatSums(input.toBigIntegerList(), answer)
+        .sumOfSmallestAndLargest()
     println("The answer to part 2 is ... $answer2")
 }
+
+// Part 2
+
+private fun findContiguousSetThatSums(numbers: List<BigInteger>, sumTo: BigInteger): List<BigInteger> {
+    var start = 0
+    var end = 1
+    var contiguousSet = numbers.subList(start, end)
+    var currentSum = contiguousSet.sum()
+
+    while (currentSum != sumTo) {
+        if (currentSum > sumTo) {
+            start += 1
+        } else if (currentSum < sumTo) {
+            end += 1
+        } else {
+            break
+        }
+        contiguousSet = numbers.subList(start, end)
+        currentSum = contiguousSet.sum()
+    }
+
+    return contiguousSet
+}
+
+private fun List<BigInteger>.sum(): BigInteger =
+    this.reduce { acc, bigInteger -> acc + bigInteger }
+
+private fun List<BigInteger>.sumOfSmallestAndLargest(): BigInteger =
+    (minOrNull() ?: BigInteger.ZERO) + (maxOrNull() ?: BigInteger.ZERO)
 
 // Part 1
 
