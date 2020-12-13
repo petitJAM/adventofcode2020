86cced7663eeb59751e58a508c83b7f1cbda9a4b
Alex Petitjean <alex@tenforwardconsulting.com>
2020-12-13 09:29:39 -0600

    Day 11 Part 2 solution

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -20,27 +20,74 @@ fun day11() {
     )
 
     val seatingArea = parseInput(input)
-    println(seatingArea.toDisplayString())
 
-    val answer = countOfOccupiedOnceStable(seatingArea)
+    val answer = countOfOccupiedOnceStable(seatingArea, SeatingArea::next)
     println("The answer to part 1 is ... $answer")
 
-    val answer2 = -1
+    val answer2 = countOfOccupiedOnceStable(seatingArea, SeatingArea::next2)
     println("The answer to part 2 is ... $answer2")
+}
+
+// Part 2
+
+private fun SeatingArea.next2(): SeatingArea = this
+    .mapIndexed { y, row ->
+        row.mapIndexed { x, seat ->
+            val point = Point(x, y)
+            when (seat) {
+                Seat.NO_SEAT -> Seat.NO_SEAT
+                Seat.EMPTY -> if (occupiedVisibleNeighborCount(point) == 0) {
+                    Seat.OCCUPIED
+                } else {
+                    Seat.EMPTY
+                }
+                Seat.OCCUPIED -> if (occupiedVisibleNeighborCount(point) >= 5) {
+                    Seat.EMPTY
+                } else {
+                    Seat.OCCUPIED
+                }
+            }
+        }
+    }
+
+private fun SeatingArea.occupiedVisibleNeighborCount(point: Point): Int =
+    this.visibleNeighbors(point).count { it == Seat.OCCUPIED }
+
+@OptIn(ExperimentalStdlibApi::class)
+private fun SeatingArea.visibleNeighbors(point: Point): List<Seat> = buildList {
+    val validY = this@visibleNeighbors.indices
+    val validX = this@visibleNeighbors[0].indices
+
+    val directions = listOf(
+        -1 to -1, 0 to -1, 1 to -1,
+        -1 to 0, 1 to 0,
+        -1 to 1, 0 to 1, 1 to 1,
+    )
+    directions.forEach { direction ->
+        var next = Point(point.x + direction.x, point.y + direction.y)
+        while (next.x in validX && next.y in validY) {
+            val seat = this@visibleNeighbors[next]
+            if (seat != Seat.NO_SEAT) {
+                add(seat)
+                break
+            }
+            next = Point(next.x + direction.x, next.y + direction.y)
+        }
+    }
 }
 
 // Part 1
 
-private fun countOfOccupiedOnceStable(seatingArea: SeatingArea): Int =
-    seatingArea.runUntilStable().occupiedCount()
+private fun countOfOccupiedOnceStable(seatingArea: SeatingArea, method: SeatingArea.() -> SeatingArea): Int =
+    seatingArea.runUntilStable(method).occupiedCount()
 
-private fun SeatingArea.runUntilStable(): SeatingArea {
+private fun SeatingArea.runUntilStable(method: SeatingArea.() -> SeatingArea): SeatingArea {
     var current = this
-    var next = current.next()
+    var next = current.method()
 
     while (current != next) {
         current = next
-        next = current.next()
+        next = current.method()
     }
 
     return current
@@ -75,23 +122,11 @@ private fun SeatingArea.next(): SeatingArea = this
         }
     }
 
-private fun SeatingArea.occupiedNeighborCount(point: Point): Int {
-    return point.neighbors()
+private fun SeatingArea.occupiedNeighborCount(point: Point): Int =
+    point.neighbors()
         .filter { it.y in this.indices && it.x in this[it.y].indices }
         .map { this[it] }
         .count { it == Seat.OCCUPIED }
-}
-
-@OptIn(ExperimentalStdlibApi::class)
-private fun SeatingArea.points(): List<Point> {
-    return buildList {
-        this@points.indices.forEach { y ->
-            this@points[y].indices.forEach { x ->
-                this.add(Point(x, y))
-            }
-        }
-    }
-}
 
 private operator fun SeatingArea.get(point: Point): Seat =
     this[point.y][point.x]
